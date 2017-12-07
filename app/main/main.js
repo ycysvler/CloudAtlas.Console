5387d309ef22a6ddce8462fde0995ed65cd6aad5
ycysvler <ycysvler@hotmail.com>
2017-12-07 22:39:39 +0800

    修改样式，添加type查询

--- app/main/main.js
+++ app/main/main.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import {HashRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
-import {Layout, Menu, Icon} from 'antd';
+import {Layout, Menu, Icon, Popover, Avatar, Button} from 'antd';
 const {Header, Footer, Sider, Content} = Layout;
 
 const SubMenu = Menu.SubMenu;
@@ -25,40 +25,57 @@ export class Main extends React.Component {
             current: e.key,
         });
     }
-
+    avatarMenu = (<div className="avatar-menu">
+        <div className="info">
+            <div className="left">
+                <Avatar size="large" style={{height:80,width:80,borderRadius:40,margin:'8px 24px 8px 8px'}}
+                        src="http://g.hiphotos.baidu.com/image/pic/item/5bafa40f4bfbfbed956ea89572f0f736afc31f20.jpg"/>
+            </div>
+            <div className="right">
+                <div>管理员</div>
+                <div>zhanghongqing@seeobject.com</div>
+                <div><Button size="small" type="primary">个人信息</Button></div>
+            </div>
+        </div>
+        <div className="toolbar"><Button>注  销</Button></div>
+    </div>)
     render() {
         return (<Layout className="main">
-                <Header className="header"></Header>
-                <Layout>
-                    <Sider width="200">
-                        <Menu mode="inline"
-                            theme="dark"
-                            onClick={this.handleClick}
-                        >
-                            <Menu.Item key="config">
-                                <Icon type="setting"/>基本配置
+                <Header className="header">
+                    <Menu mode="horizontal"
+                          onClick={this.handleClick}
+                    >
+                        <Menu.Item key="config">
+                            <Icon type="setting"/>基本配置
+                        </Menu.Item>
+                        <SubMenu title={<span><Icon type="appstore-o" /><span>数据管理</span></span>}  key="data">
+                            <Menu.Item key="user">用户</Menu.Item>
+                            <Menu.Item key="ip">IP白名单</Menu.Item>
+                            <Menu.Item key="type">图片类型</Menu.Item>
+                            <Menu.Item key="image">图片信息</Menu.Item>
+                        </SubMenu>
+                        <SubMenu title={<span><Icon type="line-chart" /><span>统计分析</span></span>}  key="statistics">
+                            <Menu.Item key="s1">统计分析1</Menu.Item>
+                            <Menu.Item key="s2">统计分析2</Menu.Item>
+                        </SubMenu>
+                        <SubMenu title={<span><Icon type="eye-o" /><span>系统监控</span></span>}  key="monitor">
+                            <Menu.Item key="m1">系统监控1</Menu.Item>
+                            <Menu.Item key="m2">系统监控2</Menu.Item>
+                        </SubMenu>
+                        <SubMenu title={<span><Icon type="eye-o" /><span>查询任务</span></span>}  key="search">
+                            <Menu.Item key="search.list">
+                                <Link to='/main/search'>查询列表</Link>
                             </Menu.Item>
-                            <SubMenu title={<span><Icon type="appstore-o" /><span>数据管理</span></span>}  key="data">
-                                <Menu.Item key="user">用户</Menu.Item>
-                                <Menu.Item key="ip">IP白名单</Menu.Item>
-                                <Menu.Item key="type">图片类型</Menu.Item>
-                                <Menu.Item key="image">图片信息</Menu.Item>
-                            </SubMenu>
-                            <SubMenu title={<span><Icon type="line-chart" /><span>统计分析</span></span>}  key="statistics">
-                                <Menu.Item key="s1">统计分析1</Menu.Item>
-                                <Menu.Item key="s2">统计分析2</Menu.Item>
-                            </SubMenu>
-                            <SubMenu title={<span><Icon type="eye-o" /><span>系统监控</span></span>}  key="monitor">
-                                <Menu.Item key="m1">系统监控1</Menu.Item>
-                                <Menu.Item key="m2">系统监控2</Menu.Item>
-                            </SubMenu>
-                            <SubMenu title={<span><Icon type="eye-o" /><span>查询任务</span></span>}  key="search">
-                                <Menu.Item key="search.list">
-                                    <Link to='/main/search'>查询列表</Link>
-                                    </Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
+                        </SubMenu>
+                    </Menu>
+                    <Popover placement="bottomRight" trigger="click" content={this.avatarMenu}>
+                        <div id="avatar">
+                            <Avatar size="large"
+                                    src="http://g.hiphotos.baidu.com/image/pic/item/5bafa40f4bfbfbed956ea89572f0f736afc31f20.jpg"/>
+                        </div>
+                    </Popover>
+                </Header>
+
                     <Layout >
                         <Router>
                             <Switch>
@@ -68,7 +85,6 @@ export class Main extends React.Component {
                             </Switch>
                         </Router>
                     </Layout>
-                </Layout>
             </Layout>
         );
     }
--- app/search/create.js
+++ app/search/create.js
@@ -2,24 +2,26 @@
  * Created by yanggang on 2017/3/6.
  */
 import React from 'react';
-import {Layout, Input, Button, Upload, message, Icon, Col, Select} from 'antd';
+import {Layout, Input, Button, Upload, message, Icon, Col, Select, Row} from 'antd';
 import moment from 'moment';
 import {SearchActions, SearchStore} from './reflux';
 
 import Config from 'config';
 
 const {Header, Footer, Sider, Content} = Layout;
+const Option = Select.Option;
 
+/*const children = ['text1','text2'];*/
 export class SearchCreate extends React.Component {
     constructor(props) {
         super(props);
         this.unsubscribe = SearchStore.listen(this.onStatusChange.bind(this));
 
-        this.state = {type:[],images:[]};
+        this.state = {type: [], images: []};
     }
 
     onStatusChange(action, data) {
-        if(action){
+        if (action) {
             console.log('create complete > ', data);
         }
     }
@@ -27,25 +29,25 @@ export class SearchCreate extends React.Component {
     componentWillUnmount() {
     }
 
-    onSearch=()=>{
-        console.log('onClick',this.state.name,this.state.type);
+    onSearch = () => {
+        console.log('onClick', this.state.name, this.state.type);
 
         SearchActions.create(
             this.state.name,
             this.state.type,
             this.state.images,
-            ["deep","color","shape","lbp"]
+            ["deep", "color", "shape", "lbp"]
         );
     }
 
-    onNameChange=(e)=>{
+    onNameChange = (e) => {
         this.state.name = e.target.value;
     }
-    onTypeChange=(e)=>{
+    onTypeChange = (e) => {
         this.state.type = [e.target.value];
     }
 
-    onChange=(info)=> {
+    onChange = (info, value) => {
         if (info.file.status !== 'uploading') {
             console.log(info.file, info.fileList);
         }
@@ -55,35 +57,60 @@ export class SearchCreate extends React.Component {
 
             console.log('info.file.response > ', info.file.name);
             console.log(images);
-            this.setState({"images":images});
+            this.setState({"images": images});
 
             message.success(`${info.file.name} file uploaded successfully`);
         } else if (info.file.status === 'error') {
             message.error(`${info.file.name} file upload failed.`);
         }
+
     }
 
     render() {
         return (
-            <div>
-                <div>name:<Input onChange={this.onNameChange} /></div>
-                <div>type;<Input onChange={this.onTypeChange} /></div>
-                <div>
-                    {this.state.images.map(function (item,index) {
-                        return <b key={index}>{item}</b>
-                    })}
-                    <Upload
-                    onChange={this.onChange}
-                    name='file'
-                    action={Config.url + '/api/search/images'}
-                >
-                    <Button>
-                        <Icon type="upload" /> Click to Upload
-                    </Button>
+            <div className="gutter-example">
+                <Row gutter={16}>
+                    <Col className="gutter-row" span={1}>
+                        <div className="gutter-box">name:</div>
+                    </Col>
+                    <Col className="gutter-row" span={5}>
+                        <div className="gutter-box">
+                            <Input onChange={this.onNameChange}/>
+                        </div>
+                    </Col>
+                    <Col className="gutter-row" span={1}>
+                        <div className="gutter-box">type;</div>
+                    </Col>
+                    <Col className="gutter-row" span={5}>
+                        <div className="gutter-box"><Select
+                            mode="multiple"
+                            onChange={this.onTypeChange}
+                            placeholder="Please select"
+                            defaultValue={['test1', 'test2']}
+                            style={{width: '100%'}}>
+                            <Option value="test1">test1</Option>
+                            <Option value="test2">test2</Option></Select></div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div className="gutter-box">
+                            {this.state.images.map(function (item, index) {
+                                return <b key={index}>{item}</b>
+                            })}
+                            <Upload
+                                onChange={this.onChange}
+                                name='file'
+                                action={Config.url + '/api/search/images'}
+                            >
+                                <Button>
+                                    <Icon type="upload"/> Click to Upload
+                                </Button>
 
-                </Upload></div>
-
-                <Button onClick={this.onSearch}>查询</Button>
+                            </Upload></div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div className="gutter-box"><Button onClick={this.onSearch}>查询</Button></div>
+                    </Col>
+                </Row>
             </div>
         );
     }
